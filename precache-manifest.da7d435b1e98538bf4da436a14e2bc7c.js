9791ba608e5e19e7ecacf0a162c9a927236d674b
张威 <zw900808@gmail.com>
2019-09-03 10:26:39 +0800

    Updates

---
+++ precache-manifest.da7d435b1e98538bf4da436a14e2bc7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea068dce2870f396f3f225878ef9115",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ade1f2ee01a182a7ca0",
+    "url": "/static/css/2.4c009698.chunk.css"
+  },
+  {
+    "revision": "933446e88a85c2bb1f3b",
+    "url": "/static/css/main.6dfa8843.chunk.css"
+  },
+  {
+    "revision": "4ade1f2ee01a182a7ca0",
+    "url": "/static/js/2.c34adc12.chunk.js"
+  },
+  {
+    "revision": "933446e88a85c2bb1f3b",
+    "url": "/static/js/main.f0d111e1.chunk.js"
+  },
+  {
+    "revision": "30cd2830f08f0bb3925e",
+    "url": "/static/js/runtime~main.9e3fe6d9.js"
+  }
+]);
